d30c6820e55cd24fc378c70a4fc38ec098155316
abhimvp <abhishek04698@gmail.com>
2024-10-09 03:00:03 -0500

    Connecting with Database SQLModel

---
+++ src/db/__init__.py
@@ -0,0 +1,2 @@
+# Inside of this package we're going to do eveyrhting that's specific to Database package
+# Means anything concerned to creating sessions & also helping us to connect to database - all that code will be inside this package
