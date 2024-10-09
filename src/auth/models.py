4dc2e37a399155df353d6ed974ffcf63b4ff189e
abhimvp <abhishekreddyboddu1@gmail.com>
2024-10-09 09:49:50 -0500

    User Authentication model & migrations setup

---
+++ src/auth/models.py
@@ -0,0 +1,29 @@
+# User Authentication model
+import uuid
+from datetime import datetime
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+
+
+class User(SQLModel,table=True):
+    __tablename__ = "users"
+    uid: uuid.UUID = Field( # to access uuid type for postgreSQL
+     sa_column= Column(
+         pg.UUID,
+         nullable=False,
+         primary_key=True,
+         default=uuid.uuid4
+      ) # to generate uuid for each row
+     ) #defining it as SQLAlchemy field
+    username: str
+    password: str
+    email: str
+    firstname: str
+    lastname: str
+    is_verified: bool = Field(default=False)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+    updated_at: datetime= Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now()))
+
+    def __repr__(self) -> str:
+       return f"<User {self.username}>"
+    
